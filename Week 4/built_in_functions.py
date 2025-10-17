9e9550015519b5f329360099e254c1a902ae8294
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-10-17 14:08:28 +0100

    Added code to demonstrate the use of built in functions len and ord

---
+++ Week 4/built_in_functions.py
@@ -0,0 +1,8 @@
+print("Program started")
+character = str(input("Please enter a standard character"))
+character2 = ord(character)
+if len(character) == 1: print("The ASCII code for: ", character, "is: ",character2)
+else: print("Too many characters")
+print("Program Ended")
+
+
