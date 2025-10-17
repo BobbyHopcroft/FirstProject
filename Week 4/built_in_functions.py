e78320bf8514b429af3f1317bcc658e82bd93ac2
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-10-17 14:48:38 +0100

    Added code to demonstrate the use of built-in functions abs range cr.

--- Week 4/built_in_functions.py
+++ Week 4/built_in_functions.py
@@ -1,8 +1,18 @@
+#print("Program started")
+#character = str(input("Please enter a standard character"))
+#character2 = ord(character)
+#if len(character) == 1: print("The ASCII code for: ", character, "is: ",character2)
+#else: print("Too many characters")
+#print("Program Ended")
+
+
 print("Program started")
-character = str(input("Please enter a standard character"))
-character2 = ord(character)
-if len(character) == 1: print("The ASCII code for: ", character, "is: ",character2)
-else: print("Too many characters")
-print("Program Ended")
+number = int(float(input("Please enter an ASCII code:")))
+number2 = abs(number)
+if number2 in range(32,127):
+    number3 = chr(number2)
+    print("The character represented by the ASCII code: ", number2, "is: ",number3)
+else: print("Number not in range")
+print("Program ended")
 
 
