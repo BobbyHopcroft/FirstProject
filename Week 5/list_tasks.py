726896aa2395d5287a8aba90327fa02396e0e7c0
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-10-24 14:46:50 +0100

    Partially completed.

--- Week 5/list_tasks.py
+++ Week 5/list_tasks.py
@@ -13,20 +13,38 @@
 #if __name__ == "__main__":
     #un_task()
 
-def movements():
-    path = ["Move forward",10,"Move backward",5,"Move left",3,"Move right",1]
-    return path
+#def movements():
+    #path = ["Move forward",10,"Move backward",5,"Move left",3,"Move right",1]
+    #return path
 
-def run_task2():
-    print("Moving...")
-    path = movements()
-    for index in range(0, len(path),2):
-        dir = path[index]
-        steps = path[index+1]
-        print(f"{dir} for {steps} steps")
+#def run_task2():
+    #print("Moving...")
+    #path = movements()
+    #for index in range(0, len(path),2):
+        #dir = path[index]
+        #steps = path[index+1]
+        #print(f"{dir} for {steps} steps")
 
-if __name__ == "__main__":
-    run_task2()
+#if __name__ == "__main__":
+    #run_task2()
+
+def directions():
+    steps = ["Move Forward", "Move Backward", "Move left", "Move right"]
+    return steps
+def menu():
+    print("Please select a direction:")
+    steps = directions()
+    for index in range(len(steps)):
+        print(f"{index}: {directions}")
+
+def run_task3():
+    menu()
+
+    if __name__ == "__main__":
+       run_task3()
+
+
+
 
 
 
