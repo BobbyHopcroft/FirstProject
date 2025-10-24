e27ba09362b5865f7d93a974c676b44c24a9808e
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-10-24 14:13:06 +0100

    Added code to demonstrate a list.

---
+++ Week 5/list_tasks.py
@@ -0,0 +1,19 @@
+def directions():
+    steps = ["Move forward", "Move Backward", "Move Left", "Move Right"]
+    # steps.append("Move forward")
+    # steps.append("Move Backward")
+    # steps.append("Move Left")
+    # steps.append("Move Right")
+    return steps
+
+def run_task():
+    steps = directions()
+    print(steps)
+
+if __name__ == "__main__":
+    run_task()
+
+
+
+
+
