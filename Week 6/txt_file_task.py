e628e76c4b6322c5b0810f0e095c2c12563c7f5e
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-10-31 14:30:57 +0000

    Added code to read a text file line by line.

--- Week 6/txt_file_task.py
+++ Week 6/txt_file_task.py
@@ -13,39 +13,58 @@ import os
 import fileinput
 
 
-def display_chars(file_path,num_chars):
-    with open(file_path) as file:
-        display_chars = file.read(num_chars)
-        print(display_chars)
+# def display_chars(file_path,num_chars):
+#     with open(file_path) as file:
+#         display_chars = file.read(num_chars)
+#         print(display_chars)
+#
+#
+#
+#
+#
+#
+# def display_lines():
+#     with open('libary.txt') as file:
+#         display_lines = file.readline()
+#         file.close()
+#         print(display_lines)
+#         return display_lines
+#
+#
+#
+#
+# def display_text(file_path):
+#     with open(file_path) as file:
+#         display_text = file.read()
+#         print(display_text)
+#         return display_text
+#
+#
+# def run_task2():
+#     display_chars('libary.txt', 5)
+#     display_lines()
+#     display_text('libary.txt')
+#
+# run_task2()
+
+
+def search(file_name):
+    print("Searching...")
+    with open(file_name) as file:
+        for line in file:
+            location = line.strip()
+            print(f"Looked in {location}")
+        print("Done")
+
+def run_task3():
+    search('libary.txt')
+
+run_task3()
 
 
 
 
 
-
-def display_lines():
-    with open('libary.txt') as file:
-        display_lines = file.readline()
-        file.close()
-        print(display_lines)
-        return display_lines
-
-
-
-
-def display_text(file_path):
-    with open(file_path) as file:
-        display_text = file.read()
-        print(display_text)
-        return display_text
-
-
-def run_task2():
-    display_chars('libary.txt', 5)
-    display_lines()
-    display_text('libary.txt')
-
-run_task2()
 
 
 
