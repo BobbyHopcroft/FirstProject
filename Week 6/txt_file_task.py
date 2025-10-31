a2c0b9a5593d0a63aff14d2d598c6dd8dc21e62a
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-10-31 14:18:31 +0000

    Added code to demonstrate how to read a text file.

--- Week 6/txt_file_task.py
+++ Week 6/txt_file_task.py
@@ -1,15 +1,51 @@
 import os
-def cwd():
-    cwd = os.getcwd()
-    print("The current working directory is:", cwd)
-    print("The directory contains the following files:")
-    print(os.listdir(cwd))
+# def cwd():
+#     cwd = os.getcwd()
+#     print("The current working directory is:", cwd)
+#     print("The directory contains the following files:")
+#     print(os.listdir(cwd))
+#
+# def run():
+#     print("Processing...")
+#
+# run()
+# cwd()
+import fileinput
 
-def run():
-    print("Processing...")
 
-run()
-cwd()
+def display_chars(file_path,num_chars):
+    with open(file_path) as file:
+        display_chars = file.read(num_chars)
+        print(display_chars)
+
+
+
+
+
+
+def display_lines():
+    with open('libary.txt') as file:
+        display_lines = file.readline()
+        file.close()
+        print(display_lines)
+        return display_lines
+
+
+
+
+def display_text(file_path):
+    with open(file_path) as file:
+        display_text = file.read()
+        print(display_text)
+        return display_text
+
+
+def run_task2():
+    display_chars('libary.txt', 5)
+    display_lines()
+    display_text('libary.txt')
+
+run_task2()
 
 
 
