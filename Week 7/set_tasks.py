c977aa36eed5225a334f7d0d7d1bee4bfa4ede87
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-11-07 14:45:47 +0000

    Partially completed task3

--- Week 7/set_tasks.py
+++ Week 7/set_tasks.py
@@ -9,13 +9,41 @@
 import fileinput
 
 
+# def observed_items():
+#     observations = []
+#     for i in range(7):
+#         observation = input("Please enter an observation")
+#         observations.append(observation)
+#     return observations
+# def run_task2():
+#     print("Counting observations...")
+#     observations = observed_items()
+#     count1 = set()
+#     for item in observations:
+#         item_count = (item, observations.count(item))
+#         count1.add(item_count)
+#     for item, total in count1:
+#         print(f"{item} observed {total} times")
+#
+#
+# run_task2()
+
 def observed_items():
     observations = []
-    for i in range(7):
+    for i in range(5):
         observation = input("Please enter an observation")
         observations.append(observation)
     return observations
-def run_task2():
+
+def remove_observations(observations):
+    print("Do you wish to remove observations?")
+    if input("yes") == "yes":
+        input("What observation do you want to be removed?")
+        observations.remove(input)
+
+def run_task3():
+    observed_items()
+    remove_observations(observed_items())
     print("Counting observations...")
     observations = observed_items()
     count1 = set()
@@ -24,9 +52,10 @@ def run_task2():
         count1.add(item_count)
     for item, total in count1:
         print(f"{item} observed {total} times")
+run_task3()
 
 
-run_task2()
+
 
 
 
