ac3af396a2d4efff37ba0263161bb2fdcbdf35f5
BOBBY HOPCROFT <0hopcb97@solent.ac.uk>
2025-11-14 13:47:51 +0000

    Added code to display multiple plots.

--- Week 8/plots.py
+++ Week 8/plots.py
@@ -10,3 +10,30 @@ import matplotlib.pyplot as plt
 #     y_values = [1,4,9,16,25]
 #     display_line(x_values,y_values)
 # run_task1()
+
+
+def small():
+    x = [3,4,4,3,3]
+    y = [3,3,4,4,3]
+    plt.plot(x,y,'o--r')
+
+
+def medium():
+    x = [2,5,5,2,2]
+    y = [2,2,5,5,2]
+    plt.plot(x,y,'o--g')
+
+def large():
+    x = [1,6,6,1,1]
+    y = [1,1,6,6,1]
+    plt.plot(x,y,'o-b')
+
+
+
+
+
+
+small()
+medium()
+large()
+plt.show()
